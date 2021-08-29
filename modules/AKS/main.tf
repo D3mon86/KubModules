5bd1c4416d5d3d580f33746675477b97f7136d51
AG <morfeus@i.ua>
2021-08-29 20:04:26 +0300

    updates to config files

--- modules/AKS/main.tf
+++ modules/AKS/main.tf
@@ -3,10 +3,28 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
+#Ideally from terraform recomendations this should be moved to main.
+provider "azurerm" {
+  version = "=2.13.0"
 
+  features {
+  }
+}
+
+provider "helm" {
+  version = "1.2.2"
+  kubernetes {
+    host = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks_rg.kube_config[0].client_key)
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_rg.kube_config[0].client_certificate)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_rg.kube_config[0].cluster_ca_certificate)
+    load_config_file       = false
+  }
+}
 #Main Resource Group 
 resource "azurerm_resource_group" "aks_rg" {
-  name     = "${var.resource_group_name}-rg"
+  name     = "rg-${var.resource_group_name}"
   location = "West Europe"
 
   tags = {
@@ -15,15 +33,15 @@ resource "azurerm_resource_group" "aks_rg" {
 }
 # Create Virtual Network
 resource "azurerm_virtual_network" "aksvnet" {
-  name                = "aks-network"
+  name                = "aks-${var.network_name}"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
-  address_space       = var.address_space                                        ["10.0.0.0/8"]
+  address_space       = var.address_space                                        #["10.0.0.0/8"]
 }
 
 # Create a Subnet for AKS
 resource "azurerm_subnet" "aks-default" {
-  name                 = "aks-default-subnet"
+  name                 = "aks-${var.subnet_name}"
   virtual_network_name = azurerm_virtual_network.aksvnet.name
   resource_group_name  = azurerm_resource_group.aks_rg.name
   address_prefixes     = var.subnet_adress_prefixes                    # ["10.240.0.0/16"]
@@ -32,7 +50,7 @@ resource "azurerm_subnet" "aks-default" {
 
 # Creates the Kubernetes cluster.                      
 resource "azurerm_kubernetes_cluster" "akscluster" {
-  name                = "${var.cluster_name}-aks"
+  name                = "aks-${var.cluster_name}"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
   dns_prefix          = "${var.dns_prefix}-k8s"
@@ -55,5 +73,15 @@ resource "azurerm_kubernetes_cluster" "akscluster" {
 
   tags = {
     environment = "Demo"
+  }
+}
+#Create Nginx Ingress Controler 
+resource "helm_release" "ingress" {
+  name  = "ingress"
+  chart = "stable/nginx-ingress"
+
+  set {
+    name  = "rbac.create"
+    value = "true"
   }
 }
---
+++ modules/AKS/variables.tf
@@ -0,0 +1,40 @@
+variable "appId" {
+  description = "Azure Kubernetes Service Cluster service principal"
+}
+
+variable "password" {
+  description = "Azure Kubernetes Service Cluster password"
+}
+variable "cluster_name" {
+  description = "Azure Kubernetes Service Cluster Name"
+}
+
+variable "dns_prefix" {
+  description = "Azure Kubernetes Service Cluster Dns Prefix"
+}
+variable "address_space" {
+  description = "Azure Kubernetes Service Cluster Adress Space"
+}
+
+variable "network_name" {
+  description = "Azure Kubernetes Service Cluster Network Name"
+}
+variable "vm_size" {
+  description = "Azure Kubernetes Service Cluster VM Size"
+}
+
+variable "os_disk_size_gb" {
+  description = "Azure Kubernetes Service Cluster Disk Size"
+}
+variable "enable_role_based_access" {
+  description = "Azure Kubernetes Service Cluster Enable Role based access"
+}
+variable "subnet_adress_prefixes" {
+  description = "Azure Kubernetes Service Cluster subnet address prefixes"
+}
+variable "subnet_name" {
+  description = "Azure Kubernetes Service Cluster subnet name"
+}
+variable "resource_group_name" {
+  description = "Azure Kubernetes Service Cluster resource group name"
+}
